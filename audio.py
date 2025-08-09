a6622675569f510db02dbd2a809f31a06837dffe
rexchoppers <connorbrady@codox.uk>
2025-08-09 21:57:44 +0100

    ADDED Audio file

---
+++ audio.py
@@ -0,0 +1,62 @@
+audio_mapping = {
+    "0": {
+        "audio": "resources/zero.mp3",
+        "cutoff": 1000
+    },
+    "1": {
+        "audio": "resources/one.mp3",
+        "cutoff": 1000
+    },
+    "2": {
+        "audio": "resources/two.mp3",
+        "cutoff": 1000
+    },
+    "3": {
+        "audio": "resources/three.mp3",
+        "cutoff": 1000
+    },
+    "4": {
+        "audio": "resources/four.mp3",
+        "cutoff": 1000
+    },
+    "5": {
+        "audio": "resources/five.mp3",
+        "cutoff": 1000
+    },
+    "6": {
+        "audio": "resources/six.mp3",
+        "cutoff": 1000
+    },
+    "7": {
+        "audio": "resources/seven.mp3",
+        "cutoff": 1000
+    },
+    "8": {
+        "audio": "resources/eight.mp3",
+        "cutoff": 1000
+    },
+    "9": {
+        "audio": "resources/nine.mp3",
+        "cutoff": 1000
+    },
+    "A": {
+        "audio": "resources/alfa.mp3",
+        "cutoff": 1000
+    },
+
+}
+
+
+
+
+"""
+F1YNE
+"""
+def append_mission_id(audio, mission_id):
+    # Split the mission_id into individual characters
+    mission_id_chars = list(mission_id)
+
+    for char in mission_id_chars:
+
+
+    return audio
