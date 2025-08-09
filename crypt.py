b368fe5263485868397559f0a09fede2084a1700
rexchoppers <connorbrady@codox.uk>
2025-08-09 12:35:13 +0100

    ADDED Crypt to generate pad

--- crypt.py
+++ crypt.py
@@ -8,3 +8,14 @@ def generate_mission_id():
     mission_id = ''.join(secrets.choice(alphabet) for _ in range(5))
 
     return mission_id
+
+def generate_pad(pages=100, groups_per_page=10, group_length=10):
+    alphabet = string.ascii_uppercase
+    pad = []
+    for _ in range(pages):
+        page = []
+        for _ in range(groups_per_page):
+            group = ''.join(secrets.choice(alphabet) for _ in range(group_length))
+            page.append(group)
+        pad.append(' '.join(page))
+    return pad
