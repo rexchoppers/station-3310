eed704ce9e17b89fa8dbbaf402fdfbb0c6143ef7
rexchoppers <connorbrady@codox.uk>
2025-08-10 07:33:07 +0100

    ADDED OTP function

--- crypt.py
+++ crypt.py
@@ -2,14 +2,14 @@ import secrets
 import string
 
 
-def generate_mission_id():
+def generate_mission_id() -> str:
     alphabet = string.ascii_uppercase + string.digits
 
     mission_id = ''.join(secrets.choice(alphabet) for _ in range(5))
 
     return mission_id
 
-def generate_pad(pages=100, groups_per_page=10, group_length=5):
+def generate_pad(pages=100, groups_per_page=10, group_length=5) -> list[str]:
     digits = string.digits
 
     pad = []
@@ -20,3 +20,13 @@ def generate_pad(pages=100, groups_per_page=10, group_length=5):
             page.append(group)
         pad.append(' '.join(page))
     return pad
+
+def opt_mod(message_digits: str, pad_digits: str) -> str:
+    if len(pad_digits) < len(message_digits):
+        raise ValueError("Pad is too short for this message")
+
+    cipher_digits = []
+    for m_dig, p_dig in zip(message_digits, pad_digits):
+        s = (int(m_dig) + int(p_dig)) % 10
+        cipher_digits.append(str(s))
+    return ''.join(cipher_digits)
